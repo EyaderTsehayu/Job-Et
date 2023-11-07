d80f59984d5319c36371c826f02f9e619e30ba96
Eyader Tsehayu <eyaderts@gmail.com>
2023-11-07 12:02:42 +0300

    finish job-details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -27,13 +27,39 @@ const JobDetails = () => {
   const { data, isLoading, error, refetch } = useFetch("job-details", {
     job_id: params.id,
   });
+
+  const tabs = ["About", "Qualifcations", "Responsibilities"];
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
   const onRefresh = useCallback(() => {
     setRefreshing(true);
     refetch();
     setRefreshing(false);
   }, []);
 
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
+      case "Qualifcations":
+        return (
+          <Specifics
+            title="Qualifcations"
+            info={data[0].job_highlights?.Qualifications ?? "N/A"}
+          />
+        );
+
+      case "Responsibilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            info={data[0].job_highlights?.Responsibilities ?? "N/A"}
+          />
+        );
+    }
+  };
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -77,15 +103,21 @@ const JobDetails = () => {
               />
 
               <JobTabs
-              // tabs={tabs}
-              // activeTab={activeTab}
-              // setActiveTab={setActiveTab}
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
               />
 
-              {/* {displayTabContent()} */}
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results"
+          }
+        />
       </>
     </SafeAreaView>
   );
